89c457bb31b6ce6901c862e11d4f5a69a8d04922
Gioxgio <fantastici02@hotmail.it>
2024-11-24 18:13:16 +0100

    Make the app a PWA

---
+++ pwa/manifest.ts
@@ -0,0 +1,42 @@
+const manifestForPlugIn = {
+    registerType: 'prompt',
+    includeAssests: ['favicon.ico', "logo192.png", "logo512.svg"],
+    manifest: {
+        name: "pkmn-collect-tool",
+        short_name: "pkmn-collect-tool",
+        description: "An app to catch them all",
+        icons: [{
+            src: '/logo192.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'favicon'
+        },
+        {
+            src: '/logo512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'favicon'
+        },
+        {
+            src: '/logo192.png',
+            sizes: '180x180',
+            type: 'image/png',
+            purpose: 'apple touch icon',
+        },
+        {
+            src: '/logo512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable',
+        }
+        ],
+        theme_color: '#171717',
+        background_color: '#f0e7db',
+        display: "standalone",
+        scope: '/',
+        start_url: "/",
+        orientation: 'portrait'
+    }
+}
+
+export default manifestForPlugIn;
