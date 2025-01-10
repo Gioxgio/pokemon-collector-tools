77a1f1ff339a39d21b04436897252388802b840f
Gioxgio <fantastici02@hotmail.it>
2025-01-10 11:10:06 +0100

    Update scraping logic

--- scraper/scraper.js
+++ scraper/scraper.js
@@ -2,102 +2,36 @@ import fs from "fs";
 
 async function scrapeCards() {
 
-    const BASE_URL = "https://pocket.codex.gg/api/cards/"
-    const response = await fetch(BASE_URL + "search");
+    const BASE_URL = "https://www.pokemon-zone.com/api/game/game-data"
+    const response = await fetch(BASE_URL);
     const initialData = await response.json();
 
-    const cardsUberJson = [];
-    const packsUberJson = [
-        {
-            id: "AN001_0010_00_000",
-            logo: "https://static.mana.wiki/tcgwiki-pokemonpocket/mewtwo-logo.png",
-            name: "Genetic Apex: Mewtwo",
-            totalCards: 0
-        },
-        {
-            id: "AN001_0020_00_000",
-            logo: "https://static.mana.wiki/tcgwiki-pokemonpocket/charizard-logo.png",
-            name: "Genetic Apex: Charizard",
-            totalCards: 0
-        },
-        {
-            id: "AN001_0030_00_000",
-            logo: "https://static.mana.wiki/tcgwiki-pokemonpocket/pikachu-logo.png",
-            name: "Genetic Apex: Pikachu",
-            totalCards: 0
-        },
-        {
-            id: "AP001_0010_00_000",
-            logo: "https://archives.bulbagarden.net/media/upload/c/c6/PA_Logo_Volume_1_EN.png",
-            name: "Promo Pack A Series Vol. 1",
-            totalCards: 0
-        },
-        {
-            id: "AP001_0020_00_000",
-            logo: "https://archives.bulbagarden.net/media/upload/4/44/PA_Set_Logo_EN.png",
-            name: "Promo Pack A Series Vol. 2",
-            totalCards: 0
+    const mappedPacks = initialData.data.packs.map((pack) => {
+        const totalCards = initialData.data.packCardIds.find((p) => p.packId == pack.packId).cardIds.length;
+        return {
+            id: pack.packId,
+            logo: pack.assetUrl,
+            name: pack.description,
+            totalCards: totalCards
         }
-    ];
+    });
 
-    for (const card of initialData.data.results) {
-
-        const cardUrl = BASE_URL + "keys/?keys=" + card.cardDefKey;
-        const cardResponse = await fetch(cardUrl);
-        const cardJson = await cardResponse.json();
-        const cardData = cardJson.data[0];
-
-        const imageUrl = cardData.externalId !== "PK_10_001400_00" ? cardData.displayImageUrl : "https://www.pokemon-zone.com/assets/uploads/2024/09/genetic-apex-a1-140-dugtrio.webp";
-
-        const transformedCard = {
-            id: cardData.externalId,
-            name: cardData.name,
-            description: cardData.description,
-            type: cardData.type,
-            series: cardData.series,
-            rarity: {
-                id: cardData.rarity,
-                name: cardData.rarityName
-            },
-            imgSrc: imageUrl,
-            expansion: {
-                id: cardData.expansionKey,
-                name: cardData.expansionName
-            },
-            types: cardData.types,
-            dustCost: cardData.dustCost,
-            collectionNumber: cardData.collectionNumber,
-            artists: cardData.artists,
-            variants: cardData.variants?.map(v => v.externalId),
-            foundInPacks: [...new Set(cardData.foundInPacks)],
-            hp: cardData.hp,
-            abilities: cardData.abilities,
+    const mappedCards = initialData.data.cards.map((card) => {
+        return {
+            id: card.cardId,
+            name: card.name,
+            rarityId: card.rarity,
+            imgSrc: card.illustrationUrl,
+            pokedexNumber: card.pokemon?.pokedexNumber,
+            foundInPacks: [...new Set(card.foundInPacks)],
             owned: false
         };
-
-        console.log("Downloaded ", transformedCard.name)
-
-        const originalFilename = `original/${card.cardDefKey}.json`;
-        fs.writeFileSync(originalFilename, JSON.stringify(cardData, null, 2));
-        const transformedFilename = `transformed/${card.cardDefKey}.json`
-        fs.writeFileSync(transformedFilename, JSON.stringify(transformedCard, null, 2));
-        console.log("Saved card data");
-
-        cardsUberJson.push(transformedCard);
-
-        transformedCard.foundInPacks.forEach((pid) => {
-            packsUberJson.filter(p => p.id == pid)[0].totalCards++;
-        });
-
-        if (!cardResponse.ok) {
-            console.error(`Error fetching card data for ${card.name}`);
-        }
-    }
+    });
 
     const cardsFilename = `cards.json`;
-    fs.writeFileSync(cardsFilename, JSON.stringify(cardsUberJson, null, 2));
+    fs.writeFileSync(cardsFilename, JSON.stringify(mappedCards));
     const packsFilename = `packs.json`;
-    fs.writeFileSync(packsFilename, JSON.stringify(packsUberJson, null, 2));
+    fs.writeFileSync(packsFilename, JSON.stringify(mappedPacks));
 }
 
 scrapeCards();
